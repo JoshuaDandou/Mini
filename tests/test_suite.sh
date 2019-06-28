f4afe6e6c6507d7a0f6918c6ec5e4c633e60ee76
Joshua Dandou <dandou_j@epita.fr>
2019-06-28 18:58:30 +0200

    ADD TestSuite

---
+++ tests/test_suite.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+make
+mv minishell tests/
+cd tests
+
+GREEN='\033[32m'
+NEUTRE='\033[0m'
+RED='\033[31m'
+
+echo Test. > test_1.txt
+echo "echo Test." | ./minishell > test_1_res.txt
+diff test_1.txt test_1_res.txt
+if [ "$?" -eq 0 ]; then
+  echo -e "echo echo Test. | ./minishell : ${GREEN}ok${NEUTRE}"
+else
+  echo -e "echo echo Test. | ./minishell : ${RED}ko${NEUTRE}"
+fi
+rm test_1_res.txt
+
+./minishell files/test.sh > test_1_res_1.txt
+diff test_1.txt test_1_res_1.txt
+if [ "$?" -eq 0 ]; then
+  echo -e "./minishell test.sh : ${GREEN}ok${NEUTRE}"
+else
+  echo -e "./minishell test.sh : ${RED}ko${NEUTRE}"
+fi
+rm test_1_res_1.txt
+rm test_1.txt
+
